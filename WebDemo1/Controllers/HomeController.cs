b2b596e3848eaa7833f26281f41240ef097d572c
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-01-08 08:38:28 +0100

    Add project files.

---
+++ WebDemo1/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDemo1.Models;
+
+namespace WebDemo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            var model = new RegisterModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterModel model)
+        {
+            var cardNo = model.CardNo;
+            var namn = model.Name;
+            var email = model.Email;
+
+            var fee = CalculateFee();
+            BillCard(cardNo,fee);
+            SaveToDb(namn,email,fee);
+            SendConfirmationEmail(email);
+            model.Registered = true;
+            return View(model);
+        }
+
+        private void SendConfirmationEmail(string email)
+        {
+            //Npot implemented. But we send an confirmation email
+        }
+
+        private void SaveToDb(string namn, string email, decimal fee)
+        {
+            //Not implemented but you get the idea
+            //lets say we use some ADO here!
+        }
+
+        private void BillCard(string cardNo, decimal fee)
+        {
+            //Not implemented...but you get the idea
+        }
+
+        private decimal CalculateFee()
+        {
+            if ((new DateTime(2020, 3, 30, 0, 0, 0) - DateTime.Now).TotalDays > 100)
+                return 5000;
+            return 10000;
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebDemo1/Models/RegisterModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebDemo1.Models
+{
+    public class RegisterModel
+    {
+        public string Name { get; set; }
+        public string CardNo { get; set; }
+        
+        public string Email { get; set; }
+        public bool Registered { get; set; } 
+    }
+}
